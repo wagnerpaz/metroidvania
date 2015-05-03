b1de9915caf5597631e6193c9811ef915494af92
Wagner Paz <wagner.o.paz@gmail.com>
2015-05-03 20:48:48 -0300

    Started the hero animation

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	private int attack1 = Animator.StringToHash("Attacking");
+
+	Animator a;
+
+	// Use this for initialization
+	void Start () {
+		a = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			a.SetTrigger(attack1);
+		}
+	}
+}
